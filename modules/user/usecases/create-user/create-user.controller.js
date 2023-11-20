52869ebbdf735d79b64b242877934cb1471c3a87
Giovane <giovaneaalmeida27@gmail.com>
2023-11-20 16:14:43 -0300

    feat: create CreateUserController

---
+++ modules/user/usecases/create-user/create-user.controller.js
@@ -0,0 +1,25 @@
+
+module.exports = class CreateUserController {
+  createUserService
+
+  constructor(createUserService, router) {
+    this.createUserService = createUserService;
+
+    router.post('/create-user', async (request, response) => {
+      try {
+        const body = request.body;
+        const result = await this.create(body);
+
+        response.status(200).json(result);
+      } catch (error) {
+        console.log(error)
+        response.status(error.statusCode).json(error.message);
+      }
+    })
+
+  }
+
+  async create(user) {
+    return this.createUserService.createUser(user)
+  }
+}
