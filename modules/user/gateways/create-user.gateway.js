32193672c6ef6a262406da7ef84adc728c2a8ff9
Giovane <giovaneaalmeida27@gmail.com>
2023-11-20 16:14:09 -0300

    feat: create CreateUserGateway

---
+++ modules/user/gateways/create-user.gateway.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+const BadRequestError = require('../../shared/errors/bad-request.errors');
+const BadGatewayError = require('../../shared/errors/bad-gateway.error');
+
+module.exports = class CreateUserGateway {
+
+  gatewayUrl;
+
+  constructor(gatewayUrl) {
+    this.gatewayUrl = gatewayUrl;
+  }
+
+  async create(user) {
+    try {
+
+      const response = await axios.post(`${this.gatewayUrl}/usuarios`, user);
+
+      return {
+        message: response.data.message,
+        id: response.data._id
+      };
+
+    } catch (error) {
+
+      if (error.status === axios.HttpStatusCode.BadRequest) {
+        throw new BadRequestError(error.message);
+      }
+
+      throw new BadGatewayError(error.message);
+    }
+  }
+}
